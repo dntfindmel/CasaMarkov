0f829e665b7e5194526623382bf5001399ebd9c5
Melyssa Moya <moyamelyssa@gmail.com>
2024-09-30 11:53:09 -0300

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { VitrineComponent } from "./vitrine/vitrine.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, VitrineComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
