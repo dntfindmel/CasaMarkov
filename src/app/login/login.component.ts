909d027a5d8f26b477fb7b799a52b89940e62b30
leagomes <leosocialnet@gmail.com>
2024-10-05 18:22:43 -0300

    Add: Login e cadastro, e suas rotas.

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Cliente } from '../model/cliente';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  public mensagem: String = "";
+  public obj : Cliente = new Cliente();
+  public fazerLogin(){
+    if(this.obj.email=='cm@casamarkov.com.br' &&
+        this.obj.senha=='123456'){
+          localStorage.setItem("cliente", JSON.stringify(this.obj));
+          window.location.href="./cadastro";
+    } else {
+      this.mensagem = "email ou senha invalidos";
+      localStorage.removeItem("cliente");
+    }  
+  }
+
+  public novoCadastro(){
+    localStorage.setItem("cliente", JSON.stringify(this.obj));
+    window.location.href="./cadastro";
+  }
+}
